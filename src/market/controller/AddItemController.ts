e25fbce577757a23e0468c2a968979160cece509
lucas6g <lucas.trabalho1533@gmail.com>
2022-01-18 09:01:20 -0300

    refactor: ensure ItemRepositoryInMemory returns a singleton

--- src/market/controller/AddItemController.ts
+++ src/market/controller/AddItemController.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from 'express'
-import { ItemRepositoryInMemory } from '../repositories/ItemRepositoryInMemory'
+import itemRepository from '../repositories/ItemRepositoryInMemory'
 import { AddItemUseCase } from '../usecases/add-item/AddItemUseCase'
 export class AddItemController {
     async handle(req: Request, res: Response): Promise<Response> {
         const { name, price } = req.body
 
-        const itemRepository = new ItemRepositoryInMemory()
         const addItemUseCase = new AddItemUseCase(itemRepository)
 
         const output = await addItemUseCase.execute({ name, price })
--- src/market/repositories/ItemRepositoryInMemory.ts
+++ src/market/repositories/ItemRepositoryInMemory.ts
@@ -3,14 +3,27 @@ import { ItemRepository } from './contracts/ItemRepository'
 import { v4 as uuidv4 } from 'uuid'
 import { InputDto } from '../usecases/add-item/InputDto'
 
-export class ItemRepositoryInMemory implements ItemRepository {
+class ItemRepositoryInMemory implements ItemRepository {
     private readonly items: Item[] = []
 
     async save(itemData: InputDto): Promise<Item> {
         const item = new Item(uuidv4(), itemData.name, itemData.price)
 
         this.items.push(item)
-
         return item
     }
+
+    async update(id: string, price: number): Promise<Item> {
+        console.log(this.items)
+        const index = this.items.findIndex((item) => {
+            return item.id === id
+        })
+
+        this.items[index].setPrice(price)
+        const updatedItem = this.items[index]
+
+        return updatedItem
+    }
 }
+
+export default new ItemRepositoryInMemory()
